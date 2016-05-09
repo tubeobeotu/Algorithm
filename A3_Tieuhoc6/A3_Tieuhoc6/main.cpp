1dd9479d21bf9df51c640acaf656b25e1d6e8aec
nguyenvantu <tubeobeotu123@yahoo.com>
2016-05-09 21:28:56 +0700

    //buoi3

---
+++ A3_Tieuhoc6/A3_Tieuhoc6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  A3_Tieuhoc6
+//
+//  Created by Techmaster on 5/9/16.
+//  Copyright © 2016 Techmaster. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
